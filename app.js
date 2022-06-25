dbb6dc47219d9cf4a301b105b1b87a29a5e010e3
Evan segaul <esegaul@gmail.com>
2022-06-25 19:17:43 -0400

    starting WIP

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express')
+const { PGClient } = require('pg')
+
 const app = express()
 const port = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/pub', (req, res) => {
+    const client = new PGClient()
+    await client.connect()
+    
+    const ethAddr = req.params.addr 
+    const pubKey = req.params.public_key
+
+    const queryText = 'INSERT INTO [TODO]'
+    const res = await client.query()
+
 })
 
 app.listen(port, () => {
