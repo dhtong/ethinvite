8aee0f5e9e53822b6461777f68167454e192f403
dan <dhtonge@gmail.com>
2022-06-25 18:02:58 -0400

    upload to ipfs

---
+++ upload-files.js
@@ -0,0 +1,31 @@
+var process = require('process').process;
+var minimist = require('minimist').minimist;
+var Web3Storage = require('web3.storage').Web3Storage;
+var getFilesFromPath = require('web3.storage').getFilesFromPath;
+var tmp = require('tmp');
+var fs = require('fs');
+
+async function upload(encryptedCalFilePath, encryptedAESKey) {
+  const aesFilePath = createTmpAESFileFor(encryptedAESKey);
+  return await uploadEncrypted('README.md', aesFilePath);
+}
+
+function createTmpAESFileFor(encryptedKey) {
+  const tmpobj = tmp.fileSync();
+  fs.writeFileSync(tmpobj.name, encryptedKey);
+  return tmpobj.name;
+}
+
+// filePath: tmp file path to encrypted ics file
+async function uploadEncrypted(encryptedCalFilePath, encryptedAESKeyFilePath) {
+  const storage = new Web3Storage({ process.env.WEB3_STORAGE_TOKEN });
+  const files = [];
+  const iscFile = await getFilesFromPath(encryptedCalFilePath);
+  files.push(...iscFile);
+  const aesFile = await getFilesFromPath(encryptedAESKeyFilePath);
+  files.push(...aesFile);
+
+  console.log(`Uploading ${files.length} files`);
+  const cid = await storage.put(files);
+  return cid;
+}
