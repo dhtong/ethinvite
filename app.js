b44534cde17c60e58a20f614fcd733c25b2858f1
dan <dhtonge@gmail.com>
2022-06-25 21:33:35 -0400

    asdf

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 import express from 'express'
 import bodyparser from 'body-parser'
 import { isRegistered, register } from './db.js'
-import { upload } from './upload-files.js'
+// import { upload } from './upload-files.js'
 import cors from 'cors';
+import multer from 'multer'
 
 const app = express()
 const port = process.env.PORT
 // var jsonParser = bodyparser.json()
 
-app.use(bodyparser.json({
-  type(req) {
-    return true;
-  }
-}));
-
+// app.use(bodyparser.json({
+//   type(req) {
+//     return true;
+//   }
+// }));
 
 app.use(cors({origin: '*'}));
 
@@ -36,8 +36,12 @@ app.post('/register', (req, res) => {
   // res.send("OK")
 });
 
-app.post('/ingestionmime', async (req, res) => {
-  console.log(req.body)
+app.post('/ingestion', multer().any(), (req, res) => {
+  // console.log(req.body)
+  console.log(Object.keys(req.body))
+  console.log(req.body['attachment-count'])
+  console.log(req.body['attachment-1'])
+  res.send()
   // if(req.body['attachment-1']['type'] == 'text/calendar') {
   //   filePath = req.body['attachment-1']['tempfile']
   //   await upload(filePath, "something")
