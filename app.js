6ea9832642f79e3e606f494d18ef03fdd0c1b8a7
Alec Benzer <alecbenzer@gmail.com>
2022-06-25 20:14:50 -0400

    fix

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 import express from 'express'
-import { isRegistered, register } from './db'
+import { isRegistered, register } from './db.js'
 
 const app = express()
 const port = process.env.PORT
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
 app.get('/isRegistered/:walletAddress', (req, res) => {
-  res.json(isRegistered(req.params.walletAddress));
+  res.json(false);
+  // res.json(isRegistered(req.params.walletAddress));
 });
 
-app.post('/isRegistered/:walletAddress', (req, res) => {
-  const { walletAddress, publicKey } = req.body.json();
-  register(walletAddress, publicKey);
-  res.send("OK")
+app.post('/register', (req, res) => {
+  console.log("isRegistered", req.body);
+  res.send(req.body);
+  // const { walletAddress, publicKey } = req.body;
+  // register(walletAddress, publicKey);
+  // res.send("OK")
 });
 
 app.listen(port, () => {
--- db.js
+++ db.js
@@ -1,9 +1,9 @@
-import { Client } from 'pg'
+import pg from 'pg'
 
-const client = new Client();
-await client.connect();
+const client = new pg.Client();
+// await client.connect();
 
-const publicKeyForWallet = (walletAddress) => {
+const publicKeyForWallet = async (walletAddress) => {
     const existing = await client.query(
         'SELECT wallet_address, public_key FROM public_keys WHERE wallet_address = $1::text',
         [walletAddress],
@@ -14,9 +14,9 @@ const publicKeyForWallet = (walletAddress) => {
     return existing.rows[0].public_key;
 }
 
-export const isRegistered = (walletAddress) => publicKeyForWallet(walletAddress) != null;
+export const isRegistered = async (walletAddress) => publicKeyForWallet(walletAddress) != null;
 
-export const register = (walletAddress, publicKey) => {
+export const register = async (walletAddress, publicKey) => {
     if (isRegistered(walletAddress)) {
         throw Exception(`${walletAddress} is already registered with a public key`)
     }
